802fdb4f62f1c8da61ec139c151f84eb7514adf9
Andrew Motoshin <andrew@htmlhero.ru>
2019-03-18 13:33:39 +0700

    Added ability to run server via npx

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
 const Server = require('./lib/index');
 
 const port = process.env.PORT || 8080;
 const server = new Server(port);
-server.start();
+server
+	.start()
+	.then(() => console.log(`PlayOnTV server started at port ${port}`));
