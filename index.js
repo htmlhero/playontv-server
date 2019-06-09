de250c2308d0346ddd92096fec5e148e66d9d65c
Andrew Motoshin <andrew@htmlhero.ru>
2019-06-09 14:52:30 +0700

    Added ability to set port and view server address

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const argv = require('yargs').argv;
+const ip = require('ip');
+const chalk = require('chalk');
 const Server = require('./lib/index');
 
-const port = process.env.PORT || 8080;
+const port = argv.port || process.env.PORT || 8080;
 const server = new Server(port);
 server
 	.start()
-	.then(() => console.log(`PlayOnTV server started at port ${port}`));
+	.then(() => console.log(
+		chalk`PlayOnTV server available on {yellow http://${ip.address()}:${port}}`
+	));
