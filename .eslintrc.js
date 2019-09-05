f0f51f956426f2a453ab8814134268bd048bca87
Andrew Motoshin <andrew@htmlhero.ru>
2019-09-06 00:27:09 +0700

    Update eslint config and code for it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
-const configInterfacedNode = require('eslint-config-interfaced/overrides/node');
-const configInterfacedMochaChai = require('eslint-config-interfaced/overrides/mocha-chai');
-
 module.exports = {
-	extends: 'eslint-config-interfaced',
-	rules: {
-		'no-else-return': 'off',
-		'padded-blocks': 'off',
-		'interfaced/props-order': 'off'
-	},
-	overrides: [
-		{
-			...configInterfacedNode,
-			files: ['lib/**', 'test/**', 'index.js'],
-			rules: {
-				...configInterfacedNode.rules,
-				'node/no-unpublished-require': 'off'
-			}
-		},
-		{
-			...configInterfacedMochaChai,
-			files: ['test/**']
+	extends: 'interfaced',
+	overrides: [{
+		files: ['lib/**', 'test/**', 'index.js'],
+		extends: 'interfaced/node',
+		rules: {
+			'no-else-return': 'off',
+			'padded-blocks': 'off',
+			'interfaced/props-order': 'off',
 		}
-	]
+	}, {
+		files: ['test/**'],
+		extends: 'interfaced/mocha-chai'
+	}]
 };
